1ab5a90f9e7aec2e216b31b357c7c972df366aa9
Cynthia Brown <titaniaandroid@gmail.com>
2026-04-01 18:16:35 +0000

    Revert codebase to 5e1246f and keep Copilot instructions

--- public/templates/nav.php
+++ public/templates/nav.php
@@ -6,7 +6,6 @@
         <ul class="nav__links">
             <li><a href="/" class="nav__link">Home</a></li>
             <li><a href="/portfolio.php" class="nav__link">Portfolio</a></li>
-            <li><a href="/events.php" class="nav__link">Events</a></li>
             <li><a href="/shop.php" class="nav__link">Shop</a></li>
             <li><a href="/about.php" class="nav__link">About</a></li>
         </ul>
@@ -18,7 +17,6 @@
     <div class="nav__mobile" id="mobileMenu">
         <a href="/" class="nav__mobile-link">Home</a>
         <a href="/portfolio.php" class="nav__mobile-link">Portfolio</a>
-        <a href="/events.php" class="nav__mobile-link">Events</a>
         <a href="/shop.php" class="nav__mobile-link">Shop</a>
         <a href="/about.php" class="nav__mobile-link">About</a>
     </div>
